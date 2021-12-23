a5cd64e0f482753e5f5358e457adeb4a9ad843c4
balteny <moritz.neske@hotmail.de>
2021-12-23 11:11:28 +0100

    added user loading from config file refactored code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 import requests
 from dotenv import load_dotenv
+import json
 
 
 load_dotenv()
@@ -13,26 +14,13 @@ anisearch_url = 'anisearch.de'
 
 mal_api_url = 'https://api.jikan.moe/v3'
 
+def load_users():
+    with open('users.json') as file:
+        d = json.load(file)
+        print(d["users"])
+        return d["users"]
 
-class User:
-  def __init__(self, username, user_id):
-    self.username = username
-    self.user_id = user_id
-
-
-users = [
-    User("Moritz", "nellow25"),
-    User("Marcel", "m4rcLs"),
-    User("Christopher", "hansklumberte")
-]
-
-
-def get_initial_mal_anime_list(user_id):
-    r = requests.get(mal_api_url + '/user/' + user_id + '/animelist/completed')
-    return r.json()["anime"]
-
-
-def get_more_mal_pages(user_id, page):
+def get_mal_page(user_id, page):
     r = requests.get(mal_api_url + '/user/' + str(user_id) + '/animelist/completed/' + str(page))
     return r.json()["anime"]
 
@@ -51,9 +39,9 @@ def check_data(animes, anime_id, user_id, current_page):
 
     if len(animes) == 300:
         # die API liefert max 300 Einträge pro Seite zurück
-        print("Getting data from page: " + current_page + 1)
+        print("Getting data from page: " + str(current_page + 1))
 
-        return check_data(get_more_mal_pages(user_id, current_page + 1), anime_id, user_id, current_page + 1)
+        return check_data(get_mal_page(user_id, current_page + 1), anime_id, user_id, current_page + 1)
 
     print("Did not found Anime: " + anime_id + " for user: " + user_id)
     return -1
@@ -72,13 +60,12 @@ class CustomClient(discord.Client):
             # id ab stelle 30
             anime_id = message.content[30:message.content.find('/', 30)]
             print("Anime id: " + anime_id)
-            for user in users:
-                data = get_initial_mal_anime_list(user.user_id)
-                score = check_data(data, anime_id, user.user_id, 1)
+            for user in load_users():
+                score = check_data(get_mal_page(user["user_id"], 1), anime_id, user["user_id"], 1)
                 if score == -1:
-                    await message.channel.send(user.username + ' hat diesen Anime noch nicht bewertet.')
+                    await message.channel.send(user["username"] + ' hat diesen Anime noch nicht bewertet.')
                 else:
-                    await message.channel.send(user.username + ': ' + str(score))
+                    await message.channel.send(user["username"] + ': ' + str(score))
 
 
 if __name__ == '__main__':
